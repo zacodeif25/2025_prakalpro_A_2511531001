d64fbbfa2f795acfada23fd740eca9f198209914
HP <HP@DESKTOP-3NK81DV>
2025-10-27 14:34:59 +0700

    Pekan 5 Praktikum Kelas A

---
+++ prakalpro_2025_A_251151001/src/pekan5/nestedFor1.java
@@ -0,0 +1,16 @@
+package pekan5;
+
+public class nestedFor1 {
+
+	public static void main(String[] args) {
+	
+		for (int i = 0; i <=100; i++) {
+			for ( int j = 1; j<=10; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+ 
+	}
+
+}
